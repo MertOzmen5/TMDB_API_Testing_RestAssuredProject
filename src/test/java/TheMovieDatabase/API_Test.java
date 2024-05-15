0c40bc24ac89269868ef5cab73f590c5790d5647
MertOzmen <avmertozmen93@gmail.com>
2024-05-16 00:35:40 +0300

    AddMovietoFavorites test environment has been finished.

--- src/test/java/TheMovieDatabase/API_Test.java
+++ src/test/java/TheMovieDatabase/API_Test.java
@@ -20,10 +20,10 @@ public class API_Test {
 
     RequestSpecification reqSpec;
     String authenticity_token;
-    Faker randomUreteci=new Faker();
-    String userName="";
-    String password="";
-    String url="https://api.themoviedb.org/3/account";
+    Faker randomUreteci = new Faker();
+    String userName = "";
+    String password = "";
+    String url = "https://api.themoviedb.org/3/account";
     int id;
 
 
@@ -52,15 +52,15 @@ public class API_Test {
         System.out.println("authenticity_token = " + authenticity_token);
     }
 
-   // @Test(dependsOnMethods = "PreLogin")
-    public void Login(){
-        Map<String,Object> body=new HashMap<>();
+    // @Test(dependsOnMethods = "PreLogin")
+    public void Login() {
+        Map<String, Object> body = new HashMap<>();
 
-        userName="TestTitans_Meeer.t";
-        password="123456789";
-        body.put("username",userName);
-        body.put("password",password);
-        body.put("authenticity_token",authenticity_token);
+        userName = "TestTitans_Meeer.t";
+        password = "123456789";
+        body.put("username", userName);
+        body.put("password", password);
+        body.put("authenticity_token", authenticity_token);
 
 
         given()
@@ -74,57 +74,78 @@ public class API_Test {
                 .then()
                 .statusCode(200)
 
-                ;
+        ;
     }
 
     @Test(dependsOnMethods = "PreLogin")
-    public void GetAccountDetails(){
+    public void GetAccountDetails() {
 
-        id=
-        given()
-                .spec(reqSpec)
-
-
-                .when()
-                .get(url)
+        id =
+                given()
+                        .spec(reqSpec)
 
 
-                .then()
-                .statusCode(200)
-                .extract().path("id")
-                ;
+                        .when()
+                        .get(url)
+
+
+                        .then()
+                        .statusCode(200)
+                        .extract().path("id")
+        ;
         System.out.println("id = " + id);
 
     }
 
     @Test(dependsOnMethods = "GetAccountDetails")
-    public void AddMovietoFavorites(){
-        Map<String,Object> body1=new HashMap<>();
-        String media_type="movie";
-        String media_id="6210c6bb9824c8001be3df40";
-        Boolean favorite=true;
+    public void AddMovietoFavorites() {
+        Map<String, Object> body1 = new HashMap<>();
+        String media_type = "movie";
+        String media_id = "6210c6bb9824c8001be3df40";
+        Boolean favorite = true;
 
-        body1.put("media_type",media_type);
-        body1.put("media_id",media_id);
-        body1.put("favorite",favorite);
+        body1.put("media_type", media_type);
+        body1.put("media_id", media_id);
+        body1.put("favorite", favorite);
 
         given()
                 .spec(reqSpec)
                 .body(body1)
 
                 .when()
-                .post(url+"/"+id+"/"+"favorite")
+                .post(url + "/" + id + "/" + "favorite")
 
 
                 .then()
                 .statusCode(201)
 
-                ;
+        ;
 
     }
 
     @Test(dependsOnMethods = "AddMovietoFavorites")
-    public void AddtoWatchlist(){
+    public void AddtoWatchlist() {
+        Map<String, Object> body2 = new HashMap<>();
+        String media_type = "movie";
+        String media_id = "622fd845109dec00461f0189";
+        Boolean watchlist = true;
+
+        body2.put("media_type", media_type);
+        body2.put("media_id", media_id);
+        body2.put("watchlist", watchlist);
+
+        given()
+                .spec(reqSpec)
+                .body(body2)
+
+                .when()
+                .post(url + "/" + id + "/" + "watchlist")
+
+
+                .then()
+                .statusCode(201)
+                .body("status_message", equalTo("The item/record was updated successfully."))
+        ;
 
 
     }
